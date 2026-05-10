b0041a1ca8b7215d382a547a3c2616ff2712764f
EnkhmunkhBEE <jingoat1111@gmail.com>
2026-05-11 00:13:43 +0800

    fixed

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-local-chat-key-only-for-dev'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
